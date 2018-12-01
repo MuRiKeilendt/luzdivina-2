cf894549618da9c9c4031c7cfab9a8d649d3c943
carlosalejandro98 <cabarcacalderon1998@gmail.com>
2018-12-01 14:40:13 -0300

    ok

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
-
+from django.contrib import admin
 from django.urls import path
-from .views import Home
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    path('',Home,name='home'),    
+    path('',views.Home,name='home'), 
+    path('formulario/',views.Formulario,name='formulario'),   
 ]
